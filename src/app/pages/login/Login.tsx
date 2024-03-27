64edbf46b2ad433d7381d076264f46008d802b8e
Gabriel Porto <tc.gabriel.porto@gruponc.net.br>
2024-03-26 23:31:47 -0300

    feat: char count using "useMemo"

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 export const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const emailLength = useMemo(() => {
+        return email.length + password.length;
+    }, [email]);
+
+    // useEffect(() => {
+    //     if (window.confirm('Deseja realmente logar?')) {
+    //         console.log('Logado com sucesso')
+    //     } else {
+    //         console.log('Logout')
+    //     }
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log('Email: ', email)
+    //     console.log('Password: ', password)
+    // }, [email, password]);
     
     const handleEntrar = () => {
+        console.log(email)
+        console.log(password)
     }
+
+
 
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email = {emailLength}</p>
 
                 <label>
                     <span>Email</span>

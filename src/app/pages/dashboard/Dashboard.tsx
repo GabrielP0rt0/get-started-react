e7500ee3240f94001938f19d500430f01094ffc7
Gabriel Porto <tc.gabriel.porto@gruponc.net.br>
2024-04-09 11:24:51 -0300

    feature: projeto concluido com json.server e crud básico

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,23 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-interface IListItem {
-    title: string;
-    isSelected: boolean;
-}
+import { ITarefa, TarefasService } from '../../shared/services/api/tarefas/TarefasService';
+import { ApiException } from '../../shared/services/api/ApiException';
+
+
 
 export const Dasboard = () => {
-    const [list, setList] = useState<IListItem[]>([])
+    const [list, setList] = useState<ITarefa[]>([])
+
+    useEffect(() => {
+        TarefasService.getAll()
+            .then((result) =>  {
+                if (result instanceof ApiException){
+                    alert(result.message);
+                } else {
+                    setList(result)
+                }
+            })
+    }, [])
 
     const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
         if (e.key === 'Enter') {
@@ -17,45 +28,76 @@ export const Dasboard = () => {
 
             e.currentTarget.value = '';
 
-            setList((oldList) => {
+            if (list.some((listItem) => listItem.title === value)) return;
 
-                if (oldList.some((listItem) => listItem.title === value)) return oldList;
-
-                return [...oldList, {
-                    title: value,
-                    isSelected: false
-                }]
-            })
+            TarefasService.create({title: value, isCompleted: false })
+                .then((result) => {
+                    if (result instanceof ApiException){
+                        alert(result.message);
+                    } else {
+                        setList((oldList) => {
+                            return [...oldList, result]
+                        })
+                    }
+                })
         }
+    }, [list])
+
+    const handleToggleComplete = useCallback ((id: number) => {
+        const tarefaToUpdate = list.find((tarefa) => tarefa.id === id);
+        if (!tarefaToUpdate) return;
+        
+        TarefasService.updateById(id.toString(), {
+            ...tarefaToUpdate,
+            isCompleted: !tarefaToUpdate.isCompleted,
+        })
+        .then((result) => {
+            if(result instanceof ApiException) {
+                alert(result.message)
+            } else {
+                setList(oldList => {
+                    return oldList.map(oldlistItem => {
+                        if (oldlistItem.id === id) return result
+                        return oldlistItem
+                    })
+                })
+            }}
+        )}, [list])
+
+    const handleDelete = useCallback((id: number) => {
+        TarefasService.deleteById(id.toString())
+        .then((result) => {
+            if (result instanceof ApiException){
+                alert(result.message)
+            } else {
+                setList(oldList => {
+                    return oldList.filter((oldListItem) => oldListItem.id !== id)
+                })
+            }
+        })
     }, [])
 
     return (
         <div>
             <p>Lista</p>
             
-            <p>{list.filter((listItem) => listItem.isSelected).length}</p>
+            <p>{list.filter((listItem) => listItem.isCompleted).length}</p>
             <input type="text" 
                 onKeyDown={handleInputKeyDown}
             />
 
             <ul>
                 {list.map((listItem) => {
-                    return <li key={listItem.title}>
+                    return <li>
                         <input type="checkbox" 
-                            checked={listItem.isSelected}
-                            onChange={(e) => {
-                                setList(oldList => {
-                                    return oldList.map(oldlistItem => {
-                                        const newIsSelected = oldlistItem.title === listItem.title ? !oldlistItem.isSelected : oldlistItem.isSelected;
-                                        return {
-                                            ...oldlistItem,
-                                            isSelected: newIsSelected
-                                        }
-                                    })
-                                })
-                            }}
+                            checked={listItem.isCompleted}
+                            onChange={() => {handleToggleComplete(listItem.id)}}
                         />
                         {listItem.title}
+                        <input type="button"
+                            value="Delete"
+                            onClick={() => {handleDelete(listItem.id)}}
+                        />
                         </li>
                 })}
             </ul>
